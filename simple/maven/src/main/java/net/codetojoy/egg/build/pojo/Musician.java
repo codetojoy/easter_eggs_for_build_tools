0609a8543ea09280c5f01935e7a63bc836bd3e50
Michael Easter <codetojoy@gmail.com>
2010-01-16 13:36:23 -0600

    adding simple example for Maven

---
+++ simple/maven/src/main/java/net/codetojoy/egg/build/pojo/Musician.java
@@ -0,0 +1,14 @@
+
+package net.codetojoy.egg.build.pojo;
+
+public class Musician {
+    private long id;
+
+    private String name;
+
+    public long getId() { return id; }
+    public void setId(long id ) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
